40ca93733b6d4093d422aff66f7c4a83f1af3947
hyrious <hyrious@outlook.com>
2020-03-30 00:42:48 +0800

    add Alda(test)

--- theme.js
+++ theme.js
@@ -4,12 +4,14 @@
       back: "white",
       front: "black",
       link: "blue",
+      control: "rgba(silver,.25)",
     },
     dark: {
       back: "black",
       front: "white",
       link: "aqua",
-    }
+      control: "rgba(gray,.25)",
+    },
   };
   const colors = {
     aqua: "#7fdbff",
@@ -30,10 +32,20 @@
     black: "#111111",
     silver: "#dddddd"
   };
+  function rgb(hex) {
+    hex = hex.replace(/^#/, '');
+    const f = i => parseInt(hex.substring(i * 2, i * 2 + 2), 16);
+    return Array(3).fill().map((_, i) => f(i)).join(',');
+  }
   function updateTheme(e) {
     const root = document.documentElement;
     for (const [k, v] of Object.entries(theme[e.matches ? "dark" : "light"])) {
-      root.style.setProperty(`--${k}`, colors[v]);
+      if (v.startsWith('rgba(')) {
+        const m = v.match(/rgba\((\w+),(.+)\)/);
+        root.style.setProperty(`--${k}`, `rgba(${rgb(colors[m[1]])},${m[2]})`);
+      } else {
+        root.style.setProperty(`--${k}`, colors[v]);
+      }
     }
   }
   updateTheme(prefersDarkMediaQueryList);
